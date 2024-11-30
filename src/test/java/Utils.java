08d483aedb355514d827e5b4e35d81a9bc405314
YevgenyN <evgenii.tst@gmail.com>
2024-11-30 21:23:21 +0300

    Update

--- src/test/java/Utils.java
+++ src/test/java/Utils.java
@@ -7,25 +7,35 @@ import java.util.Properties;
 public class Utils {
 
     public static ChromeOptions getChromeOptions() {
-        try {
-            Properties properties = new Properties();
-            FileInputStream input = new FileInputStream("src/test/resources/local.properties");
-            properties.load(input);
+        ChromeOptions options = new ChromeOptions();
 
-            String chromeOptions = properties.getProperty("chrome_options");
-
-            ChromeOptions options = new ChromeOptions();
-            if (chromeOptions != null && !chromeOptions.isEmpty()) {
-                String[] optionsArray = chromeOptions.split(";");
-                for (String option : optionsArray) {
-                    options.addArguments(option);
-                }
+        // Попытка получить опции из переменной окружения
+        String envChromeOptions = System.getenv("CHROME_OPTIONS");
+        if (envChromeOptions != null && !envChromeOptions.isEmpty()) {
+            String[] optionsArray = envChromeOptions.split(";");
+            for (String option : optionsArray) {
+                options.addArguments(option);
             }
-            return options;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return new ChromeOptions();
+        } else {
+            // Если переменная окружения не задана, читаем файл local.properties
+            try (FileInputStream input = new FileInputStream("src/test/resources/local.properties")) {
+                Properties properties = new Properties();
+                properties.load(input);
+
+                String fileChromeOptions = properties.getProperty("chrome_options");
+                if (fileChromeOptions != null && !fileChromeOptions.isEmpty()) {
+                    String[] optionsArray = fileChromeOptions.split(";");
+                    for (String option : optionsArray) {
+                        options.addArguments(option);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.err.println("Ошибка чтения файла local.properties. Используются стандартные настройки.");
+            }
         }
 
+        return options;
     }
+
 }
